ceeac58a9a2a8b364bcb570185db9f249df7b110
VikShah <my.ruby.courses@gmail.com>
2012-12-18 15:23:44 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScsApp::Application.config.secret_token = '1237e1606e1d8cc32a790ddf6292beb390b8720b6feaa0c0a661b25f71f1b22691c30d5be55dc2d592e1005ebd7df2e8a932ee2c991a00a238e907ba93e28459'
